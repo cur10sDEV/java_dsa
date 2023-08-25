334893a35903e3c43c80e033e20408c61016357d
cur10sDEV <specsx@protonmail.com>
2023-08-26 00:42:10 +0530

    added few backtracking problems

---
+++ algorithms/Backtracking/src/SudokuSolver.java
@@ -0,0 +1,96 @@
+public class SudokuSolver {
+    public static void main(String[] args) {
+        int[][] board = new int[][]{
+                {3, 0, 6, 5, 0, 8, 4, 0, 0},
+                {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                {0, 0, 5, 2, 0, 6, 3, 0, 0}
+        };
+        if (solve(board)) {
+            display(board);
+        }
+    }
+
+    static boolean solve(int[][] board) {
+        int n = board.length;
+        int row = 0;
+        int col = 0;
+        boolean isEmpty = false;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 0) {
+                    row = i;
+                    col = j;
+                    isEmpty = true;
+                    break;
+                }
+            }
+            // if empty cell found in a row break
+            if (isEmpty) {
+                break;
+            }
+        }
+        if (!isEmpty) {
+            // sudoku solved - no empty cells
+            return true;
+        }
+
+        // backtrack
+        for (int num = 1; num <= 9; num++) {
+            if (isSafe(board, row, col, num)) {
+                board[row][col] = num;
+                if (solve(board)) {
+                    // found the answer
+                    return true;
+                } else {
+                    // backtrack
+                    board[row][col] = 0;
+                }
+            }
+        }
+        return false;
+    }
+
+    static boolean isSafe(int[][] board, int row, int col, int num) {
+        // check row for duplicate
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] == num) {
+                return false;
+            }
+        }
+
+        // check col for duplicate
+        for (int[] nums : board) {
+            if (nums[col] == num) {
+                return false;
+            }
+        }
+
+        // check in subgrid for duplicates
+        int sqrt = (int) (Math.sqrt(board.length));
+        int rowStart = row - row % sqrt;
+        int colStart = col - col % sqrt;
+        for (int i = rowStart; i < rowStart + sqrt; i++) {
+            for (int j = colStart; j < colStart + sqrt; j++) {
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    static void display(int[][] board) {
+        for (int[] arr : board) {
+            for (int ele : arr) {
+                System.out.print(ele + " ");
+            }
+            System.out.println();
+        }
+    }
+}
