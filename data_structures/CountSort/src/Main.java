46d53a7948e7118358b35cd886dd4859332dfa48
cur10sDEV <specsx@protonmail.com>
2023-11-18 08:54:16 +0530

    CountSort with hashmap added

--- data_structures/CountSort/src/Main.java
+++ data_structures/CountSort/src/Main.java
@@ -1,8 +1,11 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
+// no comparison sorting algo
 public class Main {
-    // no comparison sorting algo
-    // works with small no.s
+
+    // works with small and positive numbers
     public static void countSort(int[] arr) {
         if (arr == null || arr.length <= 1) {
             return;
@@ -13,7 +16,7 @@ public class Main {
             max = Math.max(max, ele);
         }
 
-        // generating the freq arr
+        // generating the freq arr of length one more than the max element in the array
         // storing element's freq at their respective index i.e., ele == index
         int[] freqArr = new int[max + 1];
         for (int ele: arr) {
@@ -30,9 +33,42 @@ public class Main {
             }
         }
     }
+
+    // works with negative numbers
+    public static void countSortHash(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+
+        // calculate min and max for efficient traversal
+        // store key-value pairs in hashmap where key == ele & value == freq
+        int max = arr[0];
+        int min = arr[0];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int ele : arr) {
+            max = Math.max(max,ele);
+            min = Math.min(min,ele);
+            map.put(ele,map.getOrDefault(ele,0) + 1);
+        }
+
+        int curr = 0;
+        for (int i = min; i <= max; i++) {
+            // what if there's no key-value pair for the values in between min and max
+            // tip :- you can also use map.getOrDefault() and 0 as default value so that
+            // the loop won't run for that i value
+            if (map.get(i) != null) {
+                for (int j = 0; j < map.get(i); j++) {
+                    arr[curr] = i;
+                    curr++;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        int[] arr = {3,4,1,3,2,5,2,8};
-        countSort(arr);
+        int[] arr = {3,-1,1,-3,2,5,-2,8};
+//        countSort(arr);
+        countSortHash(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
